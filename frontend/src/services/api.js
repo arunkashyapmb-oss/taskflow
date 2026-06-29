990ffc019bccad105c11636f0c2b8c38cbcc0415
Arun Kashyap <arunkashyapmb@gmail.com>
2026-06-29 11:14:22 +0530

    Refresh token add fix code

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,20 @@
 import axios from "axios";
 
+//Maine Axios Request Interceptor use kiya hai jo har request ke saath access token Authorization header me bhejta hai.
+// Response Interceptor 401 Unauthorized ko detect karta hai. Agar access token expire ho jata hai, 
+// to refresh token ki help se naya access token generate karta hai, 
+// localStorage update karta hai aur original request automatically retry kar deta hai. 
+// Agar refresh token bhi invalid ho, to user ko logout karke login page par redirect kar deta hai.
+
 const api = axios.create({
   baseURL: "http://localhost:5000/api",
   withCredentials: true,
 });
 
 // Request Interceptor
-api.interceptors.request.use((config) => {
+api.interceptors.request.use((config) => {  // ye fuction har request ke sath call hoga, aur ye token ko request ke header me add karega
+  // ye function req. send hone se phle chalege or check karega ki localStorage me token hai ya nahi, agar hai to usko request ke header me add karega
+  
   const token = localStorage.getItem("token");
 
   if (token) {
@@ -18,10 +26,12 @@ api.interceptors.request.use((config) => {
 
 // Response Interceptor
 api.interceptors.response.use(
+  // ye res. aane ke bad chalega
   (response) => response,
 
   async (error) => {
     const originalRequest = error.config;
+    // taki againy request retry na ho, agar token refresh ho chuka hai to
 
     if (
       error.response?.status === 401 &&
@@ -65,3 +75,4 @@ api.interceptors.response.use(
 );
 
 export default api;
+
